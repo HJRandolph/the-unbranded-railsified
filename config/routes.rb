5944a738417266f18321b7449ac0659b4c70e5c6
HJRandolph <hjrandolph@gmail.com>
2021-06-29 21:18:39 -0400

    Adding articles controller and db

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "main_pages#index"
   get "/main_pages", to: "main_pages#index"
+
+  get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
