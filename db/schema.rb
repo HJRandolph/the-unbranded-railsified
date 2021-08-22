0c1238e69d030756346c9583072e07ab5f2435d6
HJRandolph <hjrandolph@gmail.com>
2021-08-22 16:00:27 -0400

    Updating blog to have the sme formatting as main site. Changed blog article URLs from id numbers to slugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_010533) do
+ActiveRecord::Schema.define(version: 2021_08_22_191146) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
   end
 
 end
