7e7ec3775c6387f12e267123992cb034ccc88ad4
HJRandolph <hjrandolph@gmail.com>
2021-12-04 23:43:13 -0500

    Beginning to wire up email form

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,21 @@
+class ContactMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.service_inquiry.subject
+  #
+  def service_inquiry(message)
+    @sender = message.name
+    @email = message.email
+
+    @request = message.request
+    
+    @conumber = message.conumber
+    @details = message.details
+
+    mail to: "contact@the-unbranded"
+  end
+end
+
+ 
