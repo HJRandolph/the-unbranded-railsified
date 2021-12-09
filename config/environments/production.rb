227d9439d4098f99860514a7bc80da450fb6b7e0
HJRandolph <hjrandolph@gmail.com>
2021-12-09 17:40:45 -0500

    Added missing h to host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,7 +120,7 @@ Rails.application.configure do
 
   config.force_ssl = true
 
-  ost = 'the-unbranded.herokuapp.com'
+  host = 'the-unbranded.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
