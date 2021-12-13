bf8b1f8263ac7aec96ca3c96c6f09a4a9fa4f40e
HJRandolph <hjrandolph@gmail.com>
2021-12-13 11:51:00 -0500

    Updating from address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -14,7 +14,7 @@ class ContactMailer < ApplicationMailer
     @conumber = message.conumber
     @details = message.details
 
-    mail to: "contact@the-unbranded.com", subject: "New Service Inquiry", from: "contact@the-unbranded.com"
+    mail to: "contact@the-unbranded.com", subject: "New Service Inquiry"
   end
 end
 
